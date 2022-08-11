92a8a2428cec8fa3087e3a4d201ac74832b30dd7
Freezing Moon <m.becraft1234@gmail.com>
2022-08-11 12:23:58 -0400

    Count shots for 10sec

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-const gun = document.querySelector('.luxe');
+let shots = 1;
+let startTime = 0;
+// console.log(typeof(startTime));
+
+function start(startTime) {
+
+}
+
+const gun = document.querySelector('#luxe');
 gun.addEventListener('click', function(e) {
-  console.log(e.target.value);
+  // console.log(e);
+  // console.log(e.timeStamp);
+  
+  // console.log(startTime);
+  if (startTime > 0 && e.timeStamp < (startTime + 10000)) {
+    shots++;
+  }
+  console.log(shots);
+  
+  if (startTime === 0) {
+    startTime = e.timeStamp;
+  } else {return;}
+  
 });
